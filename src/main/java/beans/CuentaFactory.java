1ce6e70f23e6471eb4c117273017dcfe5bb42ed2
TorreManias <correosinnombre02@gmail.com>
2022-08-06 18:08:17 -0600

    Bean and factory (wip) for accounting counts

---
+++ src/main/java/beans/CuentaFactory.java
@@ -0,0 +1,20 @@
+package beans;
+
+import java.util.ArrayDeque;
+
+import org.apache.poi.util.NotImplemented;
+
+public class CuentaFactory {
+
+	@NotImplemented
+	public static ArrayDeque<CuentaBean> generarColeccion () {
+		
+//		ArrayDeque<CuentaBean> coleccion = new ArrayDeque<CuentaBean>();
+//		
+//		TODO: Implementar esta función y que ponga las cuentas en ese ArrayDeque.
+//		
+//		return coleccion;
+		throw new UnsupportedOperationException();
+	}
+	
+}
